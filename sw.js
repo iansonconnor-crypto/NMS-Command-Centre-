27bb0e005de66f80c1e3fdd7bb5e99a9e6ab4676
iansonconnor-crypto <iansonconnor@icloud.com>
2026-09-16 03:19:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="nms-command-v3-1";
+const CACHE="nms-command-v4-1";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
